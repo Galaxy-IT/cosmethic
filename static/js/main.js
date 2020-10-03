eefd1113140e8fd67aa5ba9a252859463ae21001
Galaxy Front <valeriy.y@galaxy-it.net>
2020-10-03 15:00:03 +0300

    fix: add .prettierrc, fix main.js

--- static/js/main.js
+++ static/js/main.js
@@ -6,7 +6,7 @@
 
   // BURGER MENU ===============================================================================
   // add backdrop
-  const addBackdrop = (cb) => {
+  const addBackdrop = cb => {
     if ($('.menu-backdrop').length) {
       $('.menu-backdrop').remove()
     } else {
@@ -35,56 +35,54 @@
     $('.header__menu').removeClass('is-open')
   })
 
-  // SLIDERS ====================================================================================
-=======
-    // featured product slider     
-    new Swiper('#featuredProductSlider', {
+  // SLIDERS ===========================================================================================
+  // featured product slider
+  new Swiper('#featuredProductSlider', {
     slidesPerView: 'auto',
     // spaceBetween: 20,
     pagination: {
       el: '.swiper-pagination',
-      type: 'fraction',
+      type: 'fraction'
     },
     navigation: {
       nextEl: '#featuredProductSlider .swiper-button-next',
-      prevEl: '#featuredProductSlider .swiper-button-prev',
-    },
+      prevEl: '#featuredProductSlider .swiper-button-prev'
+    }
   })
-    
+
   // slide-show
   $('.thumbnails-pic').on('click', function (e) {
-    e.preventDefault();
+    e.preventDefault()
     let $this = $(this),
-        item = $this.closest('.thumbnails-item'),
-        container = $this.closest('.gallery'),
-        display = container.find('.gallery-big-pic'),
-        path = item.find('img').attr('src'),
-        duration = 100;
+      item = $this.closest('.thumbnails-item'),
+      container = $this.closest('.gallery'),
+      display = container.find('.gallery-big-pic'),
+      path = item.find('img').attr('src'),
+      duration = 100
     if (!item.hasClass('active')) {
-      item.addClass('active').siblings().removeClass('active');
+      item.addClass('active').siblings().removeClass('active')
       display.find('img').fadeOut(duration, function () {
-        $(this).attr('src', path).fadeIn(duration);
-      });
+        $(this).attr('src', path).fadeIn(duration)
+      })
     }
-  });
+  })
   // load more btn product
-  $('.product__descr-load-more').on('click', function(){
-    $('.hide-mobile').slideDown(200).addClass('slidedDown');
+  $('.product__descr-load-more').on('click', function () {
+    $('.hide-mobile').slideDown(200).addClass('slidedDown')
     $(this).fadeOut(200).addClass('hidden')
   })
   // product related slider
-  let productRelated =  new Swiper('.product__related .swiper-container', {
+  new Swiper('.product__related .swiper-container', {
     slidesPerView: 2,
     // spaceBetween: 10,
     // spaceBetween: 20,
     pagination: {
       el: '.product__related .swiper-pagination',
-      type: 'fraction',
+      type: 'fraction'
     },
     navigation: {
       nextEl: '.product__related .swiper-button-next',
-      prevEl: '.product__related .swiper-button-prev',
-    },
+      prevEl: '.product__related .swiper-button-prev'
+    }
   })
-
 })(jQuery)
